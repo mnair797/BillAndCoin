0ce4d6427e748db0ebfef1568036f36ee9cfc747
Meenakshi <mnair797@gmail.com>
2020-11-04 21:05:39 -0800

    Done with Storyboard

--- BillAndCoin/ViewController.swift
+++ BillAndCoin/ViewController.swift
@@ -7,9 +7,21 @@
 //
 
 import UIKit
+import AVFoundation
+import Speech
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var FakeButton: UIButton!
+    
+    @IBAction func ButtonPressed(_ sender: Any) {
+        let utterance = AVSpeechUtterance(string: "Bonjour!")
+        utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+        utterance.rate = 0.5
+
+        let synthesizer = AVSpeechSynthesizer()
+        synthesizer.speak(utterance)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
