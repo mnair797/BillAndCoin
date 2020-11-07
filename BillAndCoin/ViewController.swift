42981879b8a6527adca694ddd6692b2cd4eb95c2
Meenakshi <mnair797@gmail.com>
2020-11-06 18:50:38 -0800

    Added voice and Taking Image feature!

--- BillAndCoin/ViewController.swift
+++ BillAndCoin/ViewController.swift
@@ -13,7 +13,7 @@ import Alamofire
 import SwiftyJSON
 
 class ViewController: UIViewController,UINavigationControllerDelegate, UIImagePickerControllerDelegate {
-
+    
     @IBOutlet weak var takephoto: UIButton!
     
     @IBOutlet weak var pickimage: UIButton!
@@ -25,6 +25,17 @@ class ViewController: UIViewController,UINavigationControllerDelegate, UIImagePi
     @IBAction func ButtonPressed(_ sender: Any) {
         
     }
+    @IBAction func AudiotoTextButton(_ sender: Any) {
+        
+    }
+    @IBAction func repeatinstructions(_ sender: Any) {
+        let utterance = AVSpeechUtterance(string: "Hello! Please click the top left to take an image. If you would like to pick an image, click the top right. If you would like to say what you want, click the bottom left. To repeat the instruction, click the bottom right.")
+        utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+        utterance.rate = 0.5
+
+        let synthesizer = AVSpeechSynthesizer()
+        synthesizer.speak(utterance)
+    }
     @IBAction func pickimagepressed(_ sender: Any) {
         print("Button pressed")
         let vc = UIImagePickerController()
@@ -35,9 +46,20 @@ class ViewController: UIViewController,UINavigationControllerDelegate, UIImagePi
         present(vc, animated: true)
 
     }
+    
+    
     @IBAction func takephotopressed(_ sender: Any) {
-        
+        print("Button pressed")
+        let vc = UIImagePickerController()
+        vc.sourceType = .camera
+        //vc.sourceType = .photoLibrary
+        vc.allowsEditing = true
+        vc.delegate = self
+        present(vc, animated: true)
+
     }
+    
+    
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
             picker.dismiss(animated: true)
@@ -60,10 +82,10 @@ class ViewController: UIViewController,UINavigationControllerDelegate, UIImagePi
         func processFile(image: Data) {
             let imageB64 = Data(image).base64EncodedData()
             //let uploadURL = "https://3h6ys7t373.execute-api.us-east-1.amazonaws.com/Predict/03378121-5f5b-4e24-8b5b-7a029003f2a4"
-            let uploadURL = "https://svexr6i4fi.execute-api.us-east-1.amazonaws.com/Predict/93396303-c75a-49d0-9736-c540ca073768"
+            let uploadURL = "https://o9mo4v3o4c.execute-api.us-east-1.amazonaws.com/Predict/0d3bf15e-2361-429f-b38a-31e8e92371c7"
             
             AF.upload(imageB64, to: uploadURL).responseJSON { response in
-                
+
                 debugPrint(response)
                 switch response.result {
                    case .success(let responseJsonStr):
@@ -72,7 +94,7 @@ class ViewController: UIViewController,UINavigationControllerDelegate, UIImagePi
                        let predictedValue = myJson["predicted_label"].string
                        print("Saw predicted value \(String(describing: predictedValue))")
                        let predictionMessage = predictedValue!
-                        let utterance = AVSpeechUtterance(string: predictionMessage)
+                        let utterance = AVSpeechUtterance(string: "Hello")
                         utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
                         utterance.rate = 0.5
 
